5d1b6fde3895410f0e8097c4419de25f6067c835
smile-4-life <duc.tran911@hcmut.edu.vn>
2025-08-13 14:25:29 +0700

    chore(initial): upload base files containing TODO stubs

---
+++ RopeTextBuffer.cpp
@@ -0,0 +1,34 @@
+#include "RopeTextBuffer.h"
+
+// ----------------- DoublyLinkedList -----------------
+Rope::Rope() {
+    // TODO
+}
+
+Rope::~Rope() {
+    // TODO
+}
+
+// TODO: implement other methods of DoublyLinkedList
+
+// ----------------- RopeTextBuffer -----------------
+RopeTextBuffer::RopeTextBuffer() {
+    // TODO
+}
+
+RopeTextBuffer::~RopeTextBuffer() {
+    // TODO
+}
+
+// TODO: implement other methods of TextBuffer
+
+// ----------------- HistoryManager -----------------
+RopeTextBuffer::HistoryManager::HistoryManager() {
+    // TODO
+}
+
+RopeTextBuffer::HistoryManager::~HistoryManager() {
+    // TODO
+}
+
+//TODO: implement other methods of HistoryManager
---
+++ RopeTextBuffer.h
@@ -0,0 +1,122 @@
+
+#ifndef ROPE_TEXTBUFFER_H
+#define ROPE_TEXTBUFFER_H
+
+#include "main.h"
+
+/*
+    Student can define other list data structures here
+*/
+
+/**
+ * Rope (AVL-based, fixed leaf chunk size = 8)
+ */
+class Rope {
+public:
+    static const int CHUNK_SIZE = 8;
+private:
+    class Node {
+    public:
+        enum BalanceFactor { LH = 1, EH = 0, RH = -1 };
+
+    private:
+        Node* left;
+        Node* right;
+        string data;
+        int weight;
+        int height;
+        BalanceFactor balance;
+
+        Node();
+        explicit Node(const string& s);
+        bool isLeaf() const;
+    };
+
+    Node* root;
+
+    int height(Node* node) const;
+    int getTotalLength(Node* node) const;
+    void update(Node* node);
+    Node* rotateLeft(Node* x);
+    Node* rotateRight(Node* y);
+    Node* rebalance(Node* node);
+    void split(Node* node, int index, Node*& outLeft, Node*& outRight);
+    Node* concatNodes(Node* left, Node* right);
+    char charAt(Node* node, int index) const;
+    string toString(Node* node) const;
+    void destroy(Node*& node);
+
+public:
+    Rope();
+    ~Rope();
+
+    int length() const;
+    bool empty() const;
+    char charAt(int index) const;
+    string substring(int start, int length) const;
+    void insert(int index, const string& s);
+    void deleteRange(int start, int length);
+    string toString() const;
+
+#ifdef TESTING
+    friend class TestHelper;
+#endif
+};
+
+class RopeTextBuffer {
+public:
+    class HistoryManager; 
+
+private:
+    Rope rope;
+    int cursorPos;
+    HistoryManager* history;
+
+public:
+    RopeTextBuffer();
+    ~RopeTextBuffer();
+
+    void insert(const string& s);
+    void deleteRange(int length);
+    void replace(int length, const string& s);
+    void moveCursorTo(int index);
+    void moveCursorLeft();
+    void moveCursorRight();
+    int  getCursorPos() const;
+    string getContent() const;
+    int findFirst(char c) const;
+    int* findAll(char c) const;
+    void undo();
+    void redo();
+    void printHistory() const;
+#ifdef TESTING
+    friend class TestHelper;
+#endif
+};
+
+
+class RopeTextBuffer::HistoryManager {
+public:
+    struct Action {
+        string actionName;
+        int cursorBefore;
+        int cursorAfter;
+        string data;
+    };
+
+    // TODO: may provide some attributes
+
+public:
+    HistoryManager();
+    ~HistoryManager();
+    void addAction(const Action& a);
+    bool canUndo() const;
+    bool canRedo() const;
+    void printHistory() const;
+#ifdef TESTING
+    friend class TestHelper;
+#endif
+};
+
+#endif // ROPE_TEXTBUFFER_H
+
